dae68b3ce43ad3b5ea86de0322399b727fa1ed4d
Takimichi3155 <166791106+Takimichi3155@users.noreply.github.com>
2024-05-10 15:35:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "drapixstore")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001909882355"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "https://t.me/+TTiAgiM9lz5iODU9"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001909882355"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
